3f729ff94396e4bec313ebfd4efa03fcf56c0451
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-22 20:42:26 -0800

    Shows error message

--- src/components/AddEntryForm.tsx
+++ src/components/AddEntryForm.tsx
@@ -1,12 +1,12 @@
 import { useStateValue } from '../state';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const AddEntryForm: React.FC<any> = ({ onSubmit }) => {
+const AddEntryForm: React.FC<any> = ({ onSubmit, message }) => {
   const [{ diagnosisCodes }] = useStateValue();
   const [selectedDiagnosis, setSelectedDiagnosis] = useState('');
   const [date, setDate] = useState('2019-05-01');
   const [specialist, setSpecialist] = useState('Dr House');
-  const [description, setDescription] = useState('description');
+  const [description, setDescription] = useState('');
   const [healthCheckRating, setHealthCheckRating] = useState(0);
   console.log(diagnosisCodes);
   console.log(selectedDiagnosis);
@@ -18,74 +18,78 @@ const AddEntryForm: React.FC<any> = ({ onSubmit }) => {
   };
 
   return (
-    <form>
-      <h3>Add Entry Form</h3>
-      {diagnosisCodes && (
-        <select onChange={e => handleChange(e)}>
-          {diagnosisCodes.map(diagnosis => (
-            <option key={diagnosis.code} value={diagnosis.code}>
-              {diagnosis.name}
-            </option>
-          ))}
-        </select>
-      )}
+    <div>
+      <div id='LOADING'>{message}</div>
+      <form>
+        <h3>Add Entry Form</h3>
+        {diagnosisCodes && (
+          <select onChange={e => handleChange(e)}>
+            {diagnosisCodes.map(diagnosis => (
+              <option key={diagnosis.code} value={diagnosis.code}>
+                {diagnosis.name}
+              </option>
+            ))}
+          </select>
+        )}
 
-      <label>
-        Date:
-        <input
-          type='text'
-          name='date'
-          value={date}
-          onChange={e => {
-            setDate(e.target.value);
-          }}
-        />
-      </label>
-      <label>
-        Specialist:
-        <input
-          type='text'
-          name='specialist'
-          value={specialist}
-          onChange={e => {
-            setSpecialist(e.target.value);
-          }}
-        />
-      </label>
-      <label>
-        Description:
-        <input
-          type='text'
-          name='description'
-          value={description}
-          onChange={e => {
-            setDescription(e.target.value);
-          }}
-        />
-      </label>
-      <label>
-        Health Check Rating:
-        <input
-          type='number'
-          name='healthCheckRating'
-          value={healthCheckRating}
-          onChange={e => {
-            setHealthCheckRating(Number(e.target.value));
-          }}
-        />
-      </label>
+        <label>
+          Date:
+          <input
+            type='text'
+            name='date'
+            value={date}
+            onChange={e => {
+              setDate(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Specialist:
+          <input
+            type='text'
+            name='specialist'
+            value={specialist}
+            onChange={e => {
+              setSpecialist(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type='text'
+            name='description'
+            value={description}
+            onChange={e => {
+              setDescription(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Health Check Rating:
+          <input
+            type='number'
+            name='healthCheckRating'
+            value={healthCheckRating}
+            onChange={e => {
+              setHealthCheckRating(Number(e.target.value));
+            }}
+          />
+        </label>
 
-      <div>
-        <button
-          type='submit'
-          onClick={() =>
-            onSubmit({ date, specialist, description, healthCheckRating })
-          }
-        >
-          Add Entry
-        </button>
-      </div>
-    </form>
+        <div>
+          <button
+            type='submit'
+            onClick={e => {
+              e.preventDefault();
+              onSubmit({ date, specialist, description, healthCheckRating });
+            }}
+          >
+            Add Entry
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/components/PatientInfo.tsx
+++ src/components/PatientInfo.tsx
@@ -11,6 +11,7 @@ import AddEntryForm from './AddEntryForm';
 const PatientInfo: React.FC = (props: any) => {
   // https://www.carlrippon.com/typed-usestate-with-typescript/
   const [patient, setPatient] = useState<Patient | null>(null);
+  const [message, setMessage] = useState('bb');
 
   const [{ cachedPatients }, dispatch] = useStateValue();
 
@@ -20,32 +21,31 @@ const PatientInfo: React.FC = (props: any) => {
 
   console.log('cachedPatients', cachedPatients);
 
+  const fetchPatient = async (id: string, fromSubmit: boolean) => {
+    try {
+      const cachedPatient = getPatientFromCache(id);
+      console.log('cachedPatient', cachedPatient);
+      if (cachedPatient && !fromSubmit) {
+        console.log('patient from cache', cachedPatient);
+        setPatient(cachedPatient);
+        return;
+      }
+
+      const { data } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+      setPatient(data);
+      console.log('patient from api call', data);
+      dispatch(addPatientToCache(data));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   React.useEffect(() => {
     const id = props.match.params.id;
 
     //TODO maybe add type
-    const fetchPatient = async () => {
-      try {
-        const cachedPatient = getPatientFromCache(id);
-        console.log('cachedPatient', cachedPatient);
-        if (cachedPatient) {
-          console.log('patient from cache', cachedPatient);
-          setPatient(cachedPatient);
-          return;
-        }
-
-        const { data } = await axios.get<Patient>(
-          `${apiBaseUrl}/patients/${id}`
-        );
-        setPatient(data);
-        console.log('patient from api call', data);
-        dispatch(addPatientToCache(data));
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchPatient();
-  }, []);
+    fetchPatient(id, false);
+  }, [patient]);
 
   let genderIcon;
   if (!patient) {
@@ -66,9 +66,35 @@ const PatientInfo: React.FC = (props: any) => {
         `${apiBaseUrl}/patients/${patient.id}/entries`,
         entryInfo
       );
-      console.log(res);
+      console.log('res', res);
+      setMessage('good');
+      fetchPatient(patient.id, true);
     } catch (e) {
-      console.error(e);
+      setMessage(`
+      
+      
+      
+      error
+      
+      ${e.message}
+      
+      
+      
+      `);
+      console.error(
+        `
+      
+      
+      
+      error
+      
+      ${e.message}
+      
+      
+      
+      `,
+        e
+      );
     }
   };
 
@@ -90,6 +116,7 @@ const PatientInfo: React.FC = (props: any) => {
         onSubmit={(obj: object) => handleSubmit(obj)}
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         onCancel={() => {}}
+        message={message}
       />
       <Entries entries={patient.entries} />
     </div>
